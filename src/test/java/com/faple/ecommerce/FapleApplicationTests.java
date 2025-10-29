560b5f5573e0fb7e0a08792581f56fc9ed1c476b
chanboklee <chanboklee@naver.com>
2025-10-29 23:46:51 +0900

    feat : initial commit

---
+++ src/test/java/com/faple/ecommerce/FapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faple.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
