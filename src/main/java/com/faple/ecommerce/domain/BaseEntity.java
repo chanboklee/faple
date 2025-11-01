7f212ea2235949777ea69bdb9a5b23de1d60524f
chanboklee <chanboklee@naver.com>
2025-11-01 12:46:29 +0900

    feat : auditing config 설정 (생성 일시, 수정 일시 생성)

---
+++ src/main/java/com/faple/ecommerce/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.faple.ecommerce.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    /** 생성 일시 */
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    /** 수정 일시 */
+    @LastModifiedDate
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+}
