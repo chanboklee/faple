7606bfd3583fae5029b3ab92b6fb8e4c78a19d2f
chanboklee <chanboklee@naver.com>
2025-11-02 23:49:03 +0900

    chore : 코멘트 주석 정리

--- src/main/java/com/faple/ecommerce/domain/BaseEntity.java
+++ src/main/java/com/faple/ecommerce/domain/BaseEntity.java
@@ -13,12 +13,16 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    /** 생성 일시 */
+    /**
+     * 생성 일시
+     * */
     @CreatedDate
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
-    /** 수정 일시 */
+    /**
+     * 수정 일시
+     * */
     @LastModifiedDate
     @Column(name = "modified_at")
     private LocalDateTime modifiedAt;
